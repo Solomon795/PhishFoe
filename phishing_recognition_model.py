3642067811ccddb7c67bebc103415b11103c353c
Solomon795 <rebekrebeloff@gmail.com>
2024-05-21 14:06:34 +0300

    Refactoring the code, breaking into function, adding csv like output

--- phishing_recognition_model.py
+++ phishing_recognition_model.py
@@ -1,13 +1,16 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
 import logging
+
 tf.get_logger().setLevel(logging.ERROR)
 print("TensorFlow version:", tf.__version__)
 import pickle
 from nltk.util import ngrams
 import nltk
+
 nltk.data.path.append('nltk_data')
 import re
 import numpy as np
@@ -16,6 +19,8 @@ import argparse
 
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+
+
 # import logging
 
 # 1. Load the Trained Model from the Pickle File:
@@ -33,29 +38,11 @@ def load_model_email(model_path):
         tokenizer = pickle.load(handle)
     return model, tokenizer
 
+
 def load_model_url(model_path):
     with open(model_path, 'rb') as url_model_file:
         model = pickle.load(url_model_file)
     return model
-
-def load_my_model(model_path, target):
-    if target == 'email':
-        # Load the email model from the specified path
-        model = tf.keras.models.load_model(model_path)
-        # Compile the model with default loss and optimizer to avoid the warning
-        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        with open('tokenizer.pickle', 'rb') as handle:
-            tokenizer = pickle.load(handle)
-    elif target == 'url':
-        # Load the URL model from the specified path
-        with open(model_path, 'rb') as url_model_file:
-            model = pickle.load(url_model_file)
-            tokenizer = None
-    else:
-        raise ValueError(f"Invalid goal: {target}. Must be 'email' or 'url'.")
-
-    return model, tokenizer
-
 
 
 # 2. Parse Command-Line Arguments:
@@ -66,7 +53,8 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Malicious URL Detection')
     parser.add_argument('--target', type=str, choices=['url', 'email'], required=True,
                         help='Target of the detection: "url" or "email"')
-    parser.add_argument('input_list', type=str, help='Path to the file containing URLs /email chunks or direct URLs /emails')
+    parser.add_argument('input_list', type=str,
+                        help='Path to the file containing URLs /email chunks or direct URLs /emails')
     parser.add_argument('--delimiter', type=str, default='\n', help='Delimiter for separating inputs')
     args = parser.parse_args()
     return args
@@ -127,6 +115,7 @@ def preprocess_sentences_url(url):
             print(gram, "doesn't exist")
     return X
 
+
 # # #
 # 4. Predict Maliciousness for Each URL: Use your loaded model to predict whether
 # each URL is malicious or not. You can assign a score between 1 and 10 or
@@ -139,6 +128,7 @@ def predict_maliciousness_url(url, model):
     probabilities = 1 / (1 + np.exp(-model.decision_function(test)))
     return pred, probabilities
 
+
 def output_result_url(url_text, model):
     pred, probabilities = predict_maliciousness_url(url_text, model)
     if pred == 1:
@@ -146,10 +136,19 @@ def output_result_url(url_text, model):
     else:
         prediction = "Safe"
     print(f"URL: {url_text} | Prediction: {prediction} \n Malicious Score: {probabilities}\n")
+    return url_text, probabilities
+
 
 def analyze_urls(urls):
+    url_results = []
     model_path = 'malicious_url_model.pkl'
     model = load_model_url(model_path)
+    for url in urls:
+        url_text, score = output_result_url(url, model)
+        url_results.append((url_text, score))
+    return url_results
+
+
 """Stemming and Tokenizer"""
 # nltk.download('stopwords')
 stop_words = stopwords.words('english')
@@ -157,6 +156,8 @@ stemmer = SnowballStemmer('english')
 
 text_cleaning_re = "@\S+|https?:\S+|http?:\S+|[^A-Za-z0-9]:\S+|subject:\S+|nbsp"
 """"""
+
+
 def preprocess_email(text, stem=False):
     # Assuming 'preprocess' is your text cleaning function
     text = re.sub(text_cleaning_re, ' ', str(text).lower()).strip()
@@ -169,12 +170,14 @@ def preprocess_email(text, stem=False):
                 tokens.append(token)
     return " ".join(tokens)
 
+
 def tokenize_and_pad_email(email_text, tokenizer, max_length=50):
     # Tokenize the email
     tokenized_text = tokenizer.texts_to_sequences([email_text])
     # Pad the tokenized text
     padded_text = tf.keras.utils.pad_sequences(tokenized_text, maxlen=max_length)
     return padded_text
+
 
 def predict_email(email_text, model, tokenizer):
     # Preprocess the email text
@@ -185,16 +188,18 @@ def predict_email(email_text, model, tokenizer):
     prediction = model.predict(padded_text)
     return prediction
 
+
 def extract_urls_from_email(email_text):
     # Use regex to extract URLs from email body
     url_pattern = r'\b((?:https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|])'
     urls = re.findall(url_pattern, email_text)
     return urls
 
+
 def analyze_emails(emails):
     email_results = []
     email_urls = []
-    model_path = 'my_model.h5'
+    model_path = 'bi_lstm_phishing_email_TF_2.16.1.h5'
     model, tokenizer = load_model_email(model_path)
     for i, email in enumerate(emails):
         email_urls.extend(extract_urls_from_email(email))
@@ -203,22 +208,29 @@ def analyze_emails(emails):
         email_results.append((i, prediction))
         # Output the prediction
         print(f"The probability of the {i} email being malicious is: {prediction[0][0]}")
-        email_results.append((i, prediction))
-        if len(email_urls) > 0:
-            print(f"Extracted URLs from emails:\n{email_urls}\n")
+    return email_results, email_urls
 
-            # Ask user if they want to predict the maliciousness of these URLs
-            user_response = input("Do you want to predict the maliciousness of these URLs? (yes/no): ")
-            if user_response.lower() == 'yes':
-                with open('malicious_url_model.pkl', 'rb') as url_model_file:
-                    url_model = pickle.load(url_model_file)
-                # Run the program again using the extracted list of URLs
-                for url_item in email_urls:
-                    output_result_url(url_item, url_model)
-            else:
-                print("\nThank you for using PhishGuard!")
 
-    return email_results
+def format_as_csv(email_results=None, url_results=None, headers=True):
+    output = []
+
+    # Add headers if required
+    if headers:
+        output.append("ID,Type,Content,Malicious_Score")
+    if email_results is not None:
+        # Format email results
+        for email_id, score in email_results:
+            malicious_score = int(round(score.item() * 10))
+            output.append(f"{email_id},email,{malicious_score}")
+
+    if url_results is not None:
+        # Format URL results
+        for url, score in url_results:
+            malicious_score = int(round(score.item() * 10))
+            output.append(f",url,{url},{malicious_score}")
+
+    return output
+
 
 def main():
     print('''.--.
@@ -227,50 +239,29 @@ def main():
 |--| || KCK ||
 |  | |'-----'|
 |__|~')_____(''')
-    print("\nPhishGuard is your digital shield, \nmeticulously analyzing and scoring the safety of emails and URLs\n")
+    print("\nPhishFoe is your digital shield, \nmeticulously analyzing and scoring the safety of emails and URLs\n")
     # step 1
     args = parse_arguments()
     # step 2
-    if args.target == 'url':
-        model_path = 'malicious_url_model.pkl'
-    else:
-        model_path = 'my_model.h5'
-    loaded_model, tokenizer = load_my_model(model_path, args.target)
-    # step 3
     inputs = read_inputs(args.input_list, delimiter=args.delimiter)
-    email_urls = []
-    email_results = []
-
-
-
-
-    # Example usage:
-    for ind, input_item in enumerate(inputs):
-        if args.target == 'url':
-            output_result_url(input_item, loaded_model)
-        elif args.target == 'email':
-            email_urls.extend(extract_urls_from_email(input_item))
-            # Predict the email
-            prediction = predict_email(input_item, loaded_model, tokenizer)
-            email_results.append((ind, prediction))
-            # Output the prediction
-            print(f"The probability of the {ind} email being malicious is: {prediction[0][0]}")
-
-            # Print the extracted URLs
-    if len(email_urls) > 0:
-        print(f"Extracted URLs from emails:\n{email_urls}\n")
-
-        # Ask user if they want to predict the maliciousness of these URLs
-        user_response = input("Do you want to predict the maliciousness of these URLs? (yes/no): ")
-        if user_response.lower() == 'yes':
-            with open('malicious_url_model.pkl', 'rb') as url_model_file:
-                url_model = pickle.load(url_model_file)
-            # Run the program again using the extracted list of URLs
-            for url_item in email_urls:
-                output_result_url(url_item, url_model)
-        else:
-            print("\nThank you for using PhishGuard!")
-
+    # step 3
+    if args.target == 'url':
+        url_results = analyze_urls(inputs)
+        csv_output = format_as_csv(url_results=url_results, headers=True)
+    else:
+        email_results, email_urls = analyze_emails(inputs)
+        csv_output = format_as_csv(email_results=email_results, headers=True)
+        if len(email_urls) > 0:
+            print(f"Extracted URLs from emails:\n{email_urls}\n")
+            # Ask user if they want to predict the maliciousness of these URLs
+            user_response = input("Do you want to predict the maliciousness of these URLs? (yes/no): ")
+            if user_response.lower() == 'yes':
+                email_url_results = analyze_urls(email_urls)
+                csv_output = format_as_csv(email_results=email_results, url_results=email_url_results, headers=True)
+            else:
+                print("\nThank you for using PhishGuard!")
+    for line in csv_output:
+        print(line)
 
 
 if __name__ == "__main__":
